88150b1aff8022d26cd99e378749eb17981a8904
root <root@DESKTOP-8INMRB8.localdomain>
2019-10-31 18:43:54 -0400

    Added comments

--- captureNode.py
+++ captureNode.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+# Imports
 import requests
 
-ssid = input("What ssid would you like to send: ")
 
-content = {"ssid": ssid}
+#The next line will be changed later on, this is not necessary for the final version
+ssid = input("What ssid would you like to send: ") # Requests what data will be sent to server
 
-requests.post("http://127.0.0.1:3000/post", json=content)
+content = {"ssid": ssid} # Stores json data to be sent to server
+
+requests.post("http://127.0.0.1:3000/post", json=content) # Send post request
