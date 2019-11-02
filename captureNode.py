f5560a2694765d690f0895abc5190e5ee7475930
YodaByteCode <camimportantemails@gmail.com>
2019-11-02 10:03:47 -0400

    Modified to work better for addition of scapy

--- captureNode.py
+++ captureNode.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
+"""
+[summary] 
+"""
 # Imports
 import requests
+from scapy.all import sniff
+
+# Settings
+defaultAddress = "http://127.0.0.1:3000/post"
+interface = "wlp2s0"
+
+""" The next line will be changed later on, this is not necessary for the final version
+# Requests what data will be sent to server
+ssid = input("What ssid would you like to send: ")
+"""
 
 
-#The next line will be changed later on, this is not necessary for the final version
-ssid = input("What ssid would you like to send: ") # Requests what data will be sent to server
+def main():
+    while true:
+        packet = sniff(count=1, filter="wlan.ssid", iface=interface)
 
-content = {"ssid": ssid} # Stores json data to be sent to server
 
-requests.post("http://127.0.0.1:3000/post", json=content) # Send post request
+def sendSSID(ssid, addr=defaultAddress):
+    content = {"ssid": ssid}  # Stores json data to be sent to server
+
+    requests.post(addr, json=content)  # Send post request
+
+
+print("Send SSID")
+sendSSID("This one is in the logs too")
