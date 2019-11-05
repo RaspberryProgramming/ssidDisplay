2775a12171c2cb568602e837d241758b6e7de1f6
YodaByteCode <camimportantemails@gmail.com>
2019-11-04 19:23:38 -0500

    Added support for packet capture

--- captureNode.py
+++ captureNode.py
@@ -8,7 +8,7 @@ from scapy.all import sniff
 
 # Settings
 defaultAddress = "http://127.0.0.1:3000/post"
-interface = "wlp2s0"
+interface = "wlp2s0mon"
 
 """ The next line will be changed later on, this is not necessary for the final version
 # Requests what data will be sent to server
@@ -17,8 +17,11 @@ ssid = input("What ssid would you like to send: ")
 
 
 def main():
-    while true:
-        packet = sniff(count=1, filter="wlan.ssid", iface=interface)
+    while True:
+        packet = sniff(
+            count=1, filter="type mgt subtype beacon", iface=interface)  # Capture packets
+        print(packet)  # extract ssid
+        # sendSSID(ssid)
 
 
 def sendSSID(ssid, addr=defaultAddress):
@@ -27,5 +30,4 @@ def sendSSID(ssid, addr=defaultAddress):
     requests.post(addr, json=content)  # Send post request
 
 
-print("Send SSID")
-sendSSID("This one is in the logs too")
+main()
