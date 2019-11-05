7196fccc4e7fd1632c34cc031c585c19b2f651ae
YodaByteCode <camimportantemails@gmail.com>
2019-11-04 20:31:00 -0500

    Added asyncio support

--- captureNode.py
+++ captureNode.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 """
-[summary] 
+[summary]
 """
 # Imports
 import requests
-from scapy.all import sniff
+import asyncio
+from scapy.all import sniff, Dot11Beacon, Dot11Elt
 
 # Settings
-defaultAddress = "http://127.0.0.1:3000/post"
+defaultAddress = "http://127.0.0.1:3000/post"  # Address to send found ssids
+# Specifies wireless interface to capture 802.11 packets. Make sure it's in monitor mode
 interface = "wlp2s0mon"
 
-""" The next line will be changed later on, this is not necessary for the final version
-# Requests what data will be sent to server
-ssid = input("What ssid would you like to send: ")
-"""
 
-
-def main():
+async def main():
     while True:
-        packet = sniff(count=1, filter="type mgt subtype beacon", iface=interface)  # Capture pack>
-        try:
-            print(packet[0][Dot11Beacon][Dot11Elt].info.decode())  # extract ssid
-        except:
-            print(packet[0])
-        # sendSSID(ssid)
+        packet = sniff(count=1, filter="type mgt subtype beacon",
+                       iface=interface)  # Capture packets
+        ssidProcessor(packet)
 
 
+async def ssidProcessor(packet):
+    try:
+        # extract ssid
+        ssid = packet[0][Dot11Beacon][Dot11Elt].info.decode()
+        print(ssid)
+        # await  sendSSID(ssid)
+    except:
+        print(packet[0])
 
-def sendSSID(ssid, addr=defaultAddress):
+
+async def sendSSID(ssid, addr=defaultAddress):
     content = {"ssid": ssid}  # Stores json data to be sent to server
 
     requests.post(addr, json=content)  # Send post request
 
 
-main()
+asyncio.run(main())
