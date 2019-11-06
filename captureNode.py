3f20f67536c05df2f81bf0c3150f5db5b3652eda
camerin <camimportantemails@gmail.com>
2019-11-05 20:26:37 -0500

    Removed Async because it was causing issues when testing capture.

--- captureNode.py
+++ captureNode.py
@@ -4,7 +4,7 @@
 """
 # Imports
 import requests
-import asyncio
+#import asyncio
 from scapy.all import sniff, Dot11Beacon, Dot11Elt
 
 # Settings
@@ -13,27 +13,27 @@ defaultAddress = "http://127.0.0.1:3000/post"  # Address to send found ssids
 interface = "wlp2s0mon"
 
 
-async def main():
+def main():
     while True:
         packet = sniff(count=1, filter="type mgt subtype beacon",
                        iface=interface)  # Capture packets
         ssidProcessor(packet)
 
 
-async def ssidProcessor(packet):
+def ssidProcessor(packet):
     try:
         # extract ssid
         ssid = packet[0][Dot11Beacon][Dot11Elt].info.decode()
         print(ssid)
-        # await  sendSSID(ssid)
+        sendSSID(ssid)
     except:
         print(packet[0])
 
 
-async def sendSSID(ssid, addr=defaultAddress):
+def sendSSID(ssid, addr=defaultAddress):
     content = {"ssid": ssid}  # Stores json data to be sent to server
 
     requests.post(addr, json=content)  # Send post request
 
 
-asyncio.run(main())
+main()
